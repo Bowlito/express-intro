ffc02f85cdefa1bccf7927f13fc9046c549972d1
Bowlito <julien.catoio@gmail.com>
2025-09-01 09:51:43 +0200

    Solution avec Node.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import { createServer } from 'node:http';
+import 'dotenv/config'
+
+const server = createServer((req, res) => {
+    res.end("Hello world!")
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Adresse serveur : http://localhost:${PORT}`);
+
+})
